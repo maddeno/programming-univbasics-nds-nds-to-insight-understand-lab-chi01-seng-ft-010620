2702bce5c19562d64bc2e955135a2cd94be07ff3
Olivia Madden <madden.livi@gmail.com>
2019-12-30 03:36:31 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ def print_first_directors_movie_titles
   first_director = directors_database.first 
   index = 0 
   while index < first_director.length do
-    puts first_director[index][:title]
+    puts first_director[index][title]
     index += 1
   end
 end
